819962bf515e1ad35f07bb1241de2456604720ce
Karim Mortabit <karim.mortabit@gmail.com>
2016-01-20 00:36:32 +0100

    Presentation assets are now stored and retrieved from S3

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,35 @@
 class PagesController < ApplicationController
   layout 'application'
+  require 'aws-sdk'
   def index
 
   end
 
   def presentation
     @source_path = Rails.root.join('app', 'assets', 'images', 'presentation')
+    # Because you're never going to commit credentials to source. Please.
+    if Rails.env.production?
+      Aws.config.update({
+          :access_key_id => ENV['S3_KEY'],
+          :secret_access_key => ENV['S3_SECRET'],
+      })
+    else
+      creds = JSON.load(File.read('secrets.json'))
+      Aws.config.update({
+          :access_key_id => creds['AccessKeyId'],
+          :secret_access_key => creds['SecretAccessKey'],
+      })
+    end
+    bucket_name = 'cbc-assets'
+    s3 = Aws::S3::Client.new(region:'eu-central-1')
+    resp = s3.list_objects(bucket: 'cbc-assets')
+    @images = []
+    # resp.contents.each_with_index do |object, index|
+    #   @images << object.key unless index == 0
+    #   # puts "#{object.key} => #{object.value}"
+    # end
+    @images = resp.contents.map(&:key)
+    @images.delete_at(0)
   end
 
   def membres
