af8973b9293f0383c965969fe9d7be0620c2501c
Karim Mortabit <karim.mortabit@gmail.com>
2016-01-27 02:50:12 +0100

    Press link and articles from Back-office

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -46,6 +46,8 @@ class PagesController < ApplicationController
 
   def presse
     @source_path = Rails.root.join('app', 'assets', 'images', 'presse')
+    @articles = PressArticle.all
+    @links = PressLink.all
   end
 
   def contact
